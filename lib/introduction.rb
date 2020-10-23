307d1c4b7873685f961b0cc68e7a9964b05bea7e
RayHonore9 <raynaldhonore@gmail.com>
2020-10-23 15:18:43 -0400

    commit tree 7d9aab8adb0914a5bb477ea631c0dd6db84af7d5

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -4,3 +4,6 @@ def introduction(ronny)
     puts "Hi, my name is #{ronny}."
 end
 
+def introduction_with_language(ronny, javascript)
+    puts "Hi, my name is #{ronny} and I am learning to program in #{javascript}."
+end
